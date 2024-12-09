041b0d02a8ea1950d773ff5e8997ea391ed05110
ccmenezes <claudiacm.ti@gmail.com>
2024-12-09 16:33:51 +0000

    Installed eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+import typescript from "@typescript-eslint/eslint-plugin";
+import playwright from "eslint-plugin-playwright";
+import typescriptParser from "@typescript-eslint/parser";
+const { configs: typescriptConfigs } = typescript;
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": typescript,
+      "playwright": playwright
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    rules: {
+      ...typescriptConfigs.recommended.rules,
+      ...playwright.configs['flat/recommended'].rules,
+      "no-console": "warn",
+    }
+  },
+  {
+    ignores: [ ".github", "node_modules", "playwright-report", "test-results" ]
+  },
+];
