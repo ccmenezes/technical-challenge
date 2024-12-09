ab70e9b271ccb1d586c07c022f580f10d0a0b2a3
ccmenezes <claudiacm.ti@gmail.com>
2024-12-09 23:00:42 +0000

    Installed and configured prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,33 +1,39 @@
-// import globals from "globals";
-// import pluginJs from "@eslint/js";
-// import tseslint from "typescript-eslint";
-import typescript from "@typescript-eslint/eslint-plugin";
-import playwright from "eslint-plugin-playwright";
-import typescriptParser from "@typescript-eslint/parser";
+import typescript from '@typescript-eslint/eslint-plugin';
+import playwright from 'eslint-plugin-playwright';
+import typescriptParser from '@typescript-eslint/parser';
 const { configs: typescriptConfigs } = typescript;
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    files: ["**/*.ts", "**/*.tsx"],
+    files: ['**/*.ts', '**/*.tsx'],
     plugins: {
-      "@typescript-eslint": typescript,
-      "playwright": playwright
+      '@typescript-eslint': typescript,
+      playwright: playwright,
     },
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-      }
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
     },
     rules: {
       ...typescriptConfigs.recommended.rules,
       ...playwright.configs['flat/recommended'].rules,
-      "no-console": "warn",
-    }
+      'no-console': 'warn',
+    },
   },
   {
-    ignores: [ ".github", "node_modules", "playwright-report", "test-results" ]
+    ignores: [
+      '.github',
+      'node_modules',
+      'playwright-report',
+      'test-results',
+      '.vscode/',
+      '.idea',
+      '*.json',
+      '.DS_Store',
+    ],
   },
 ];
