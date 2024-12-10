8d16338e92a483337af537eb24753a6d6c41eaba
Claudia Menezes <claudiacm.ti@gmail.com>
2024-12-10 01:24:55 +0000

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,8 +17,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never', outputFolder: 'output/playwright-report' }]],
-  outputDir: 'output/test-results',
+  reporter: 'html',
+  //reporter: [['html', { open: 'never', outputFolder: 'output/playwright-report' }]],
+  //outputDir: 'output/test-results',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
