22052048553409d920eb71c3197fb0daa8f52e2f
ccmenezes <claudiacm.ti@gmail.com>
2024-12-11 21:44:23 +0000

    Update homepage.po.ts

--- page-objects/homepage.po.ts
+++ page-objects/homepage.po.ts
@@ -10,7 +10,7 @@ export class Homepage {
 
   constructor(page: Page) {
     this.page = page;
-    this.productsContainer = page.locator('div > .container > .card'); // a generic locator, the data id changes according filters
+    this.productsContainer = page.locator('div > .container > .card'); // a generic locator, because the data id changes according to filters
     this.sortSelect = page.locator('[data-test="sort"]');
     this.searchArea = page.locator('[data-test="search-query"]');
     this.searchButton = page.locator('[data-test="search-submit"]');
