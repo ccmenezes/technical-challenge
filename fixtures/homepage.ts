633642c119f72a1572a4e2c1ccd2ae8bf25332bd
ccmenezes <claudiacm.ti@gmail.com>
2024-12-11 13:24:42 +0000

    Improve test scernarios, add fixture folder

---
+++ fixtures/homepage.ts
@@ -0,0 +1,35 @@
+export const PRICE_DESC = [
+  /.*89.55/,
+  /.*86.71/,
+  /.*80.19/,
+  /.*73.59/,
+  /.*66.54/,
+  /.*61.16/,
+  /.*58.48/,
+  /.*48.41/,
+  /.*46.50/,
+];
+export const PRICE_ASC = [/.*3.55/, /.*3.95/, /.*4.65/, /.*4.92/, /.*5.55/, /.*6.25/, /.*7.23/, /.*7.99/, /.*9.17/];
+export const NAME_DESC = [
+  /Wood Saw/,
+  /Wood Carving Chisels/,
+  /Washers/,
+  /Tool Cabinet/,
+  /Thor Hammer/,
+  /Tape Measure 7.5m/,
+  /Tape Measure 5m/,
+  /Swiss Woodcarving Chisels/,
+  /Super-thin/,
+];
+
+export const NAME_ASC = [
+  /Adjustable Wrench/,
+  /Angled Spanner/,
+  /Belt Sander/,
+  /Bolt Cutters/,
+  /Chisels Set/,
+  /Circular Saw/,
+  /Claw Hammer/,
+  /Claw Hammer with Fiberglass Handle/,
+  /Claw Hammer with Shock Reduction Grip/,
+];
--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { PRICE_DESC, PRICE_ASC, NAME_ASC, NAME_DESC } from '../fixtures/homepage';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
@@ -7,27 +8,54 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Sorting', () => {
   test('Should sorting products by name', async ({ page }) => {
-    const PRODUCTS_CONTAINER_BY_NAME_DESC =
-      ' Wood Saw $12.18 Wood Carving Chisels $45.23 Washers $3.55 Tool Cabinet $86.71 Thor Hammer $11.14 Tape Measure 7.5m $7.23 Tape Measure 5m $12.91 Swiss Woodcarving Chisels $22.96 Super-thin Protection Gloves $38.45';
+    const PRODUCTS_CONTAINER = page.locator('[data-test="sorting_completed"] > .card');
+    const NUMBER_OF_CARDS = 9;
 
+    // Sort by name A - Z
+    await page.locator('[data-test="sort"]').selectOption('name,asc');
+    await page.locator('[data-test="search-submit"]').click();
+    // Count the total number of rows
+    await expect(PRODUCTS_CONTAINER).toHaveCount(NUMBER_OF_CARDS);
+    // Verifify the sort asc by name
+    for (let index = 0; index < NAME_ASC.length; index++) {
+      await expect(PRODUCTS_CONTAINER.nth(index)).toHaveText(NAME_ASC[index]);
+    }
+
+    // Sort by name Z - A
     await page.locator('[data-test="sort"]').selectOption('name,desc');
     await page.locator('[data-test="search-submit"]').click();
-    await expect(page.locator('[data-test="sorting_completed"]')).toHaveText(PRODUCTS_CONTAINER_BY_NAME_DESC);
+    // Count the total number of rows
+    await expect(PRODUCTS_CONTAINER).toHaveCount(NUMBER_OF_CARDS);
+    // Verifify the sort desc by name
+    for (let index = 0; index < NAME_DESC.length; index++) {
+      await expect(PRODUCTS_CONTAINER.nth(index)).toHaveText(NAME_DESC[index]);
+    }
   });
 
   test('Should sorting products by price', async ({ page }) => {
-    const PRODUCTS_CONTAINER_BY_PRICE_DESC =
-      ' Drawer Tool Cabinet $89.55 Tool Cabinet $86.71 Circular Saw $80.19 Belt Sander $73.59 Cordless Drill 24V $66.54 Leather toolbelt $61.16 Sheet Sander $58.48 Bolt Cutters $48.41 Cordless Drill 12V $46.50';
+    const SORT_SELECT = '[data-test="sort"]';
+    const SUBMIT_BUTTON = '[data-test="search-submit"]';
+    const PRODUCTS_CONTAINER = page.locator('[data-test="sorting_completed"] > .card');
+    const NUMBER_OF_CARDS = 9;
 
-    const PRODUCTS_CONTAINER_BY_PRICE_ASC =
-      ' Washers $3.55 Flat-Head Wood Screws $3.95 M4 Nuts $4.65 Phillips Screwdriver $4.92 Nuts and bolts $5.55 Screws $6.25 Tape Measure 7.5m $7.23 Cross-head screws $7.99 Slip Joint Pliers $9.17';
+    // Sort by price high to low
+    await page.locator(SORT_SELECT).selectOption('price,desc');
+    await page.locator(SUBMIT_BUTTON).click();
+    // Count the total number of rows
+    await expect(PRODUCTS_CONTAINER).toHaveCount(NUMBER_OF_CARDS);
+    // Verifify the sort desc by price
+    for (let index = 0; index < PRICE_DESC.length; index++) {
+      await expect(PRODUCTS_CONTAINER.nth(index)).toHaveText(PRICE_DESC[index]);
+    }
 
-    await page.locator('[data-test="sort"]').selectOption('price,desc');
-    await page.locator('[data-test="search-submit"]').click();
-    await expect(page.locator('[data-test="sorting_completed"]')).toHaveText(PRODUCTS_CONTAINER_BY_PRICE_DESC);
-
-    await page.locator('[data-test="sort"]').selectOption('price,asc');
-    await page.locator('[data-test="search-submit"]').click();
-    await expect(page.locator('[data-test="sorting_completed"]')).toHaveText(PRODUCTS_CONTAINER_BY_PRICE_ASC);
+    // Sort by price low to high
+    await page.locator(SORT_SELECT).selectOption('price,asc');
+    await page.locator(SUBMIT_BUTTON).click();
+    // Count the total number of rows
+    await expect(PRODUCTS_CONTAINER).toHaveCount(NUMBER_OF_CARDS);
+    // Verifify the sort asc by price
+    for (let index = 0; index < PRICE_ASC.length; index++) {
+      await expect(PRODUCTS_CONTAINER.nth(index)).toHaveText(PRICE_ASC[index]);
+    }
   });
 });
