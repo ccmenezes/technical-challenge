e8e3b160dfed553db0e1669ce6c915304325bd6f
ccmenezes <claudiacm.ti@gmail.com>
2024-12-17 12:50:18 +0000

    Update test timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   testDir: './tests',
+  timeout: 50000,
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
