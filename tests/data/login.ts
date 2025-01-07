9565adb94bd0222b070787d1c83d8cecc8e55dd5
ccmenezes <claudiacm.ti@gmail.com>
2025-01-07 13:03:54 +0000

    Delete remaining old data

--- tests/data/login.ts
+++
@@ -1,8 +0,0 @@
-/* Login Page */
-//Tests
-
-//Should login with user valid credentials
-export const ACCOUNT_PATH = /account/;
-
-//Should throw an error message for invalid credentials
-export const INCORRECT_CREDENTIALS_MSG = 'Invalid email or password';
