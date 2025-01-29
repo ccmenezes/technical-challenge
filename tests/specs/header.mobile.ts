6b5dac05a7a1b680a2bbe459f3e4cc709fa339e3
ccmenezes <claudiacm.ti@gmail.com>
2025-01-29 18:09:35 +0000

    First commit

--- tests/specs/header.mobile.ts
+++ tests/specs/header.mobile.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
-//Page objects
 import { HeaderMobile } from '../page-objects/header.mobile.po';
-//Helpers
 import { MENU_LIST, CATEGORY_LIST, SUBMENU_COUNT, CATEGORY_COUNT } from '../data/header.mobile.json';
 
 test.describe('HeaderMobile - Top menu', () => {
+  let topMenu: HeaderMobile;
+
   test.beforeEach(async ({ page }) => {
-    const topMenu = new HeaderMobile(page);
+    topMenu = new HeaderMobile(page);
     await topMenu.goto();
   });
 
   test('Should expand main menu subcategories', async ({ page }) => {
-    const topMenu = new HeaderMobile(page);
+    topMenu = new HeaderMobile(page);
     await topMenu.menuButton.click();
     for (const menuItem of await topMenu.menuList.all()) {
       await expect(menuItem).toHaveCount(SUBMENU_COUNT);
@@ -20,7 +20,7 @@ test.describe('HeaderMobile - Top menu', () => {
   });
 
   test('Should expand the categories menu', async ({ page }) => {
-    const topMenu = new HeaderMobile(page);
+    topMenu = new HeaderMobile(page);
     await topMenu.menuButton.click();
     await topMenu.categoryMenu.click();
     await expect(topMenu.categoryList).toHaveCount(CATEGORY_COUNT);
